5710bf8ce2d91dbd1bf9bcb5c7880a43ef4c78be
Hunney-ji <138511825+Hunney-ji@users.noreply.github.com>
2024-10-20 06:43:25 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -6,7 +6,11 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin:["https://syntax-2024.vercel.app/"],
+  methods:["POST","GET"],
+  credentials:true
+}));
 app.use(express.json()); 
 
 // MongoDB Atlas connection string
