7908d63362c5ab14bb6fd605d54a0e6cedb33778
Hunney-ji <138511825+Hunney-ji@users.noreply.github.com>
2024-10-20 06:46:14 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -7,7 +7,7 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors({
-  origin:["https://syntax-2024.vercel.app/"],
+  origin:["https://syntax-2024.vercel.app"],
   methods:["POST","GET"],
   credentials:true
 }));
